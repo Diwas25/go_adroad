359ea948ed6dee53bdcdc6fed277b3f735e3339a
Diwas <diwasarya25@gmail.com>
2017-12-07 00:38:26 +0530

    added mew ost css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
-
-    gulp.task('default',function(){
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),   
+browserSync = require('browser-sync').create();
+   
+gulp.task('default',function(){
     console.log("Hey i created a gulp file today");
 });
 
@@ -9,16 +15,30 @@ gulp.task('html',function(){
     console.log("Imagine some useful changes done in HTML");
 });
 gulp.task('styles',function(){
-    console.log("Imagine SAAS or PostCSS task running");
+   return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+       .pipe(gulp.dest('./app/temp/styles'));
 });
 
+
 gulp.task('watch',function(){
-    watch('./app/index.html',function(){
-        gulp.start('html');
+	
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir:"app"
+		}
+	});
+	watch('./app/index.html',function(){
+       browserSync.reload(); 
     });
-});
-gulp.task('watch',function(){
     watch('./app/assets/styles/*.css',function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
+	
+});
+
+gulp.task('cssInject',['styles'],function(){
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
 });
